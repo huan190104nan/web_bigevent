052818c35fff07e3e0e9758eae1b7ec30325af4c
huan <1597794917@qq.com>
2020-08-04 21:23:17 +0800

    完成了项目

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,78 +1,161 @@
 $(function () {
-  // 定义查询数据对象
-  //pagenum 页码值默认第一页 pagesize每页显示的数据条数默认2
-  var data = {
-    pagenum: 1,
-    pagesize: 2,
-    cate_id: "",
-    state: "",
-  };
-  initTable();
-  //发起请求获取到文章列表数据
-  function initTable() {
-    $.ajax({
-      method: "GET",
-      url: "/my/article/list",
-      data: data,
-      success: function (res) {
-        console.log(res);
-        if (res.status !== 0) {
-          return layui.layer.msg("获取列表失败");
-        }
-        //使用模板引擎渲染数据
-        var strhtml = template("tpl_table", res);
-        $("tbody").html(strhtml);
-      },
-    });
-  }
-
-  //定义时间过滤器
-  template.defaults.imports.dataTormat = function (date) {
-    var date = new Date(date);
-    var y = date.getFullYear();
-    var m = padZero(date.getMonth() + 1);
-    var d = padZero(date.getDate());
-    var h = padZero(date.getHours());
-    var mm = padZero(date.getMinutes());
-    var ss = padZero(date.getSeconds());
-    return y + "-" + m + "-" + d + " " + h + ":" + mm + ":" + ss;
-  };
-
-  //定义补零函数
-  function padZero(n) {
-    return n < 10 ? "0" + n : n;
-  }
-
-  getCate();
-
-  //发起请求获取到文章类别数据
-  function getCate() {
-    $.ajax({
-      method: "GET",
-      url: "/my/article/cates",
-      success: function (res) {
-        if (res.status !== 0) {
-          return layui.layer.msg("获取文章失败");
-        }
-
-        var str = template("tpl_cate", res);
-        $("[name=cate_id").html(str);
-        //使用layui重新渲染页面
-        layui.form.render();
-      },
-    });
-  }
-
-  //监听表单的删选事件
-  $("#form_search").on("submit", function (e) {
-    e.preventDefault();
-    //获取到当前选中的文章分类和发布状态的值
-    var cate_id = $("[name=cate_id]").val();
-    var state = $("[name=state]").val();
-    //将获取到的数据重新赋值给data
-    data.cate_id = cate_id;
-    data.state = state;
-    //重新渲染表格
+    // 定义查询数据对象
+    //pagenum 页码值默认第一页 pagesize每页显示的数据条数默认2
+    var data = {
+        pagenum: 1,
+        pagesize: 2,
+        cate_id: "",
+        state: "",
+    };
     initTable();
-  });
+    //发起请求获取到文章列表数据
+    function initTable() {
+        $.ajax({
+            method: "GET",
+            url: "/my/article/list",
+            data: data,
+            success: function (res) {
+                // console.log(res);
+                if (res.status !== 0) {
+                    return layui.layer.msg("获取列表失败");
+                }
+                //使用模板引擎渲染数据
+                var strhtml = template("tpl_table", res);
+                $("tbody").html(strhtml);
+                renderPage(res.total);
+            },
+        });
+    }
+
+    //定义时间过滤器
+    template.defaults.imports.dataTormat = function (date) {
+        var date = new Date(date);
+        var y = date.getFullYear();
+        var m = padZero(date.getMonth() + 1);
+        var d = padZero(date.getDate());
+        var h = padZero(date.getHours());
+        var mm = padZero(date.getMinutes());
+        var ss = padZero(date.getSeconds());
+        return y + "-" + m + "-" + d + " " + h + ":" + mm + ":" + ss;
+    };
+
+    //定义补零函数
+    function padZero(n) {
+        return n < 10 ? "0" + n : n;
+    }
+
+    getCate();
+
+    //发起请求获取到文章类别数据
+    function getCate() {
+        $.ajax({
+            method: "GET",
+            url: "/my/article/cates",
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg("获取文章失败");
+                }
+
+                var str = template("tpl_cate", res);
+                $("[name=cate_id").html(str);
+                //使用layui重新渲染页面
+                layui.form.render();
+            },
+        });
+    }
+
+    //监听表单的删选事件
+    $("#form_search").on("submit", function (e) {
+        e.preventDefault();
+        //获取到当前选中的文章分类和发布状态的值
+        var cate_id = $("[name=cate_id]").val();
+        var state = $("[name=state]").val();
+        //将获取到的数据重新赋值给data
+        data.cate_id = cate_id;
+        data.state = state;
+        //重新渲染表格
+        initTable();
+    });
+
+    //分页渲染函数
+    function renderPage(total) {
+        //调用lauui中的分页方法
+        layui.laypage.render({
+            elem: "layPageBox", //分页区盒子的id
+            count: total, //数据总数
+            limit: data.pagesize, //每页显示的条数
+            curr: data.pagenum, //起始页
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits: [2, 3, 5, 10],
+            // 当分页被切换时触发，函数返回两个参数：obj（当前分页的所有选项值）、first（是否首次，一般用于初始加载的判断）
+            jump: function (obj, first) {
+                //将最新的页码值和分页值赋值给data
+                data.pagenum = obj.curr
+                data.pagesize = obj.limit
+                //防止进入调用initTable进入死循环.当发生点击的时候再调用
+                if (!first) {
+                    initTable();
+                }
+            }
+        });
+    }
+
+    //给文章中的删除按钮添加点击事件
+    //动态生成需要委托添加
+    $('tbody').on('click', '.btn_del', function () {
+        //获取删除按钮的个数
+        var btnNum = $('.btn_del').length
+        console.log(btnNum);
+        //获取点击按钮的id
+        var id = $(this).attr('data_id')
+        //添加弹出层
+        layui.layer.confirm('确认删除?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //发起请求删除对应的id的文章在渲染页面
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/delete/' + id,
+                success: function (res) {
+                    if (res.status !== 0) {
+                        return layui.layer.msg('删除数据失败')
+                    }
+                    layui.layer.msg(res.message)
+                    //判断当前页面的按钮数,大于1保持原来的页面,等于1,删除之后跳转到前一页并且加载前一页的数据,页码值-1,但当页面值是1的时候不再减保持当前的页码值
+                    if (btnNum === 1) {
+                        data.pagenum = data.pagenum === 1 ? 1 : data.pagenum - 1
+                    }
+                    //重新渲染数据
+                    initTable();
+                    //do something
+                    layui.layer.close(index);
+                }
+            })
+
+        })
+
+    })
+
+    //给编辑按钮添加事件
+    $('tbody').on('click', '.btn_edit', function () {
+
+        var id = $(this).attr('data-id')
+        localStorage.setItem('id', id)
+        // console.log(id);
+        location.href = 'change.html'
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
---
+++ assets/js/article/change.js
@@ -0,0 +1,151 @@
+$(function () {
+    var $image = "";
+    var layer = layui.layer
+    var form = layui.form
+    // 初始化富文本编辑器
+    initEditor()
+    initCate()
+    //发起请求获取到文章类别
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章失败')
+                }
+
+                var id = localStorage.getItem('id')
+
+                $.ajax({
+                    method: "GET",
+                    url: '/my/article/' + id,
+                    success: function (res_data) {
+                        for (let i = 0; i < res.data.length; i++) {
+                            if (res.data[i].Id == res_data.data.cate_id) {
+                                res.data[i].is_select = true;
+                            } else {
+                                res.data[i].is_select = false;
+                            }
+
+                        }
+
+
+                        console.log(res_data);
+                        //获取成功之后将数据渲染到页面
+                        var strhtml = template('tel_select', res)
+                        $('[name=cate_id]').html(strhtml)
+                        //渲染表单中的数据
+                        form.render()
+
+                        $('[name=title]').val(res_data.data.title);
+                        $('[name=cate_id]').val(res_data.data.cate_id);
+                        $('[name=content]').val(res_data.data.content);
+                        $('#image_cover').attr("src", "http://ajax.frontend.itheima.net" + res_data.data.cover_img);
+
+                        image_cover_fun();
+                    }
+
+                })
+
+            }
+        })
+    }
+
+    function image_cover_fun() {
+        // 1. 初始化图片裁剪器
+        $image = $('#image_cover')
+        console.log($image);
+
+        // 2. 裁剪选项
+        var options = {
+            aspectRatio: 400 / 280,
+            preview: '.img-preview'
+        }
+
+        // 3. 初始化裁剪区域
+        $image.cropper(options)
+    }
+
+
+    //给选择封面添加点击事件
+    $('#chooseImage').on('click', function () {
+        $('#upfile').click()
+    })
+    //监听文件上传事件
+    $('#upfile').on('change', function (e) {
+        //获取到上传文件的列表
+        var fileLists = e.target.files
+        console.log(fileLists);
+        //如果用户没有选择文件则退出
+        if (fileLists.length == 0) return
+        //选择了文件将新的图片添加到裁剪区
+        var newIMageUrl = URL.createObjectURL(fileLists[0])
+        // 为裁剪区域重新设置图片
+        $image
+            .cropper('destroy') // 销毁旧的裁剪区域
+            .attr('src', newIMageUrl) // 重新设置图片路径
+            .cropper(options) // 重新初始化裁剪区域
+    })
+
+    //默认发布状态是已发布
+    var states = '已发布'
+    //给存为草稿按钮添加点击事件
+    $('#btnsave2').on('click', function () {
+        states = '草稿'
+    })
+
+    // 监听表单提交事件
+    $('#formUP').on('submit', function (e) {
+        //阻止表单默认提交
+        e.preventDefault()
+        //定义需要传入的参数
+        var fd = new FormData($(this)[0])
+        //将发布状态的数据添加到fd中
+        fd.append('state', states)
+
+        //设置cover_img 裁剪图片的数据(转换成文件)
+        $image.cropper('getCroppedCanvas', {
+                // 创建一个 Canvas 画布
+                width: 400,
+                height: 280
+            })
+            .toBlob(function (blob) {
+                // 将 Canvas 画布上的内容，转化为文件对象
+                //  将文件对象，存储到 fd 中
+                fd.append('cover_img', blob)
+                //发起ajax请求
+                publishArticle(fd)
+            })
+
+
+        function publishArticle(fd) {
+            $.ajax({
+                method: 'POST',
+                url: '/my/article/edit',
+                data: fd,
+                contentType: false,
+                processData: false,
+                success: function (res) {
+                    if (res.status !== 0) {
+                        return layer.msg('发表文章失败')
+                    }
+                    //成功之后跳转到文章列表页面
+                    layer.msg('发表文章成功')
+                    location.href = 'art_list.html'
+                }
+
+
+            })
+        }
+
+    })
+
+
+
+
+
+
+
+
+})
